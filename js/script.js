d93e4a1728bb123826c822dcb22a40ae292e3bb2
skyDog <michaylov.vlad169@list.ru>
2022-09-03 16:17:43 +0300

    one-practice

---
+++ js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+/* jshint -W097 */
+
+const numberOfFolms = +prompt("Сколько фильмов вы уже посмотрели", "");
+
+
+const personalMovieDB = {
+    count: numberOfFolms,
+    movies: {
+
+    },
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+const questionLastFilm = prompt("Один из последних просмотренных фильмов", ""),
+    questionFilmScore = prompt("На сколько оцените его?", ""),
+    questionLastFilmTwo = prompt("Один из последних просмотренных фильмов", ""),
+    questionFilmScoreTwo = prompt("Один из последних просмотренных фильмов", "");
+
+personalMovieDB.movies[questionLastFilm] = questionFilmScore;
+personalMovieDB.movies[questionLastFilmTwo] = questionFilmScoreTwo;
+
+console.log(personalMovieDB);
